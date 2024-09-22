6f48e48d5bfc02c0abfafe81b105b593ffde98da
Chris Kim <chriskim@stream.local>
2024-09-21 20:49:59 -0700

    setup

--- apps/angular-task/src/app/app.routes.ts
+++ apps/angular-task/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
+import { UserDetailsComponent } from './features/users/user-details/user-details.component';
+import { UsersComponent } from './features/users/users.component';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: 'user/:id', component: UserDetailsComponent },
+    {
+        path: '',
+        component: UsersComponent,
+        providers: [UsersFacade, provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+            provideEffects(UsersEffects)],
+    },
+];
