cbcbde79f44d297cfc2f24b7af7a64d5c28b4b16
Chris Kim <chriskim@stream.local>
2024-09-30 15:48:42 -0700

    component tests

--- apps/angular-task/src/app/features/users/user-details/user-details.component.spec.ts
+++ apps/angular-task/src/app/features/users/user-details/user-details.component.spec.ts
@@ -2,20 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserDetailsComponent } from './user-details.component';
 import { UsersFacade } from '../../../+state/users.facade';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, Subject } from 'rxjs';
+import { userOne } from '../../../mocks/users/test-users.mocks';
+import { UsersEntity } from '../../../+state/users.models';
 describe('UserComponent', () => {
 
     let component: UserDetailsComponent;
     let fixture: ComponentFixture<UserDetailsComponent>;
 
+    const mockFacade = {
+
+        init: jest.fn(),
+        toggleFav: jest.fn(),
+        setFilterText: jest.fn(),
+        selectedUser$: new Observable<{ user: UsersEntity }>()
+
+    };
     beforeEach(async () => {
 
-        const mockFacade = {
-
-            init: jest.fn(),
-            toggleFav: jest.fn(),
-            setFilterText: jest.fn(),
-
-        };
         await TestBed.configureTestingModule({
             imports: [UserDetailsComponent, BrowserAnimationsModule],
             providers: [{ provide: UsersFacade, useValue: mockFacade }]
@@ -30,6 +34,21 @@ describe('UserComponent', () => {
     it('should create', () => {
 
         expect(component).toBeTruthy();
+
+    });
+
+    it('should load the selected user', () => {
+
+        const selectedUser$ = new Subject<{ user: UsersEntity }>();
+        mockFacade.selectedUser$ = selectedUser$.asObservable();
+        selectedUser$.next({ user: userOne });
+
+        mockFacade.selectedUser$.subscribe(() =>  {
+
+            const title = fixture.nativeElement.querySelector('.mat-title-medium').innerText;
+            expect(title).toBe('Leanne Graham');
+
+        });
 
     });
 
--- apps/angular-task/src/app/features/users/users.component.spec.ts
+++ apps/angular-task/src/app/features/users/users.component.spec.ts
@@ -4,20 +4,23 @@ import { UsersFacade } from '../../+state/users.facade';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, Subject } from 'rxjs';
+import { UsersEntity } from '../../+state/users.models';
+import { userOne, userTwo } from '../../mocks/users/test-users.mocks';
 describe('UsersComponent', () => {
 
     let component: UsersComponent;
     let fixture: ComponentFixture<UsersComponent>;
+    const mockFacade = {
 
+        init: jest.fn(),
+        toggleFav: jest.fn(),
+        setFilterText: jest.fn(),
+        filteredUsers$: new Observable< UsersEntity[] >()
+
+    };
     beforeEach(async () => {
 
-        const mockFacade = {
-
-            init: jest.fn(),
-            toggleFav: jest.fn(),
-            setFilterText: jest.fn(),
-
-        };
         await TestBed.configureTestingModule({
             imports: [UsersComponent, BrowserAnimationsModule],
             providers: [
@@ -35,6 +38,22 @@ describe('UsersComponent', () => {
     it('should create', () => {
 
         expect(component).toBeTruthy();
+
+    });
+    it('should load  users', () => {
+
+        const filteredUsers$ = new Subject<UsersEntity[]>();
+        mockFacade.filteredUsers$ = filteredUsers$.asObservable();
+        filteredUsers$.next([userOne, userTwo]);
+
+        mockFacade.filteredUsers$.subscribe(() =>  {
+
+            const firstTitle = fixture.nativeElement.querySelector('.header-title')[0].innerText;
+            const secondTitle = fixture.nativeElement.querySelector('.header-title')[1].innerText;
+            expect(firstTitle).toBe('Leanne Graham');
+            expect(secondTitle).toBe('Ervin Howell');
+
+        });
 
     });
 
