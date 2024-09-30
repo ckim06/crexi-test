57f93c52f092669fdb16622d1d42e9e26e41441b
Chris Kim <chriskim@stream.local>
2024-09-29 22:26:07 -0700

    more tests

--- apps/angular-task/src/app/+state/users.reducer.spec.ts
+++ apps/angular-task/src/app/+state/users.reducer.spec.ts
@@ -28,6 +28,30 @@ describe('Users Reducer', () => {
             expect(result.ids.length).toBe(1);
 
         });
+        it('should toggle favorite', () => {
+
+            const users = [userOne, userTwo];
+            UsersActions.loadUsersSuccess({ users });
+            const favAction = UsersActions.toggleFavorite({ user: userOne });
+            const favresult: UsersState = usersReducer(initialUsersState, favAction);
+
+            expect(favresult.entities[userOne.id]?.isFavorite).toBe(true);
+
+        });
+
+        it('selectUserSuccess should maintain the existing favorite', () => {
+
+            const users = [userOne, userTwo];
+            UsersActions.loadUsersSuccess({ users });
+            const favAction = UsersActions.toggleFavorite({ user: userOne });
+            const result: UsersState = usersReducer(initialUsersState, favAction);
+
+            UsersActions.selectUserSuccess({ user: userOne });
+
+            expect(result.loaded).toBe(true);
+            expect(result.entities[userOne.id]?.isFavorite).toBe(true);
+
+        });
 
     });
 
--- apps/angular-task/src/app/+state/users.selectors.spec.ts
+++ apps/angular-task/src/app/+state/users.selectors.spec.ts
@@ -1,28 +1,25 @@
-import { UsersEntity } from './users.models';
+import { userOne, userTwo } from '../mocks/users/test-users.mocks';
+
 import { usersAdapter, UsersPartialState, initialUsersState } from './users.reducer';
 import * as UsersSelectors from './users.selectors';
 
 describe('Users Selectors', () => {
 
     const ERROR_MSG = 'No Error Available';
-    const getUsersId = (it: UsersEntity) => it.id;
-    const createUsersEntity = (id: string, name = '') =>
-        ({
-            id,
-            name: name || `name-${id}`,
-        }) as UsersEntity;
 
     let state: UsersPartialState;
 
     beforeEach(() => {
 
         state = {
-            users: usersAdapter.setAll([createUsersEntity('PRODUCT-AAA'), createUsersEntity('PRODUCT-BBB'),
-                createUsersEntity('PRODUCT-CCC')], {
+            users: usersAdapter.setAll([userOne, userTwo], {
                 ...initialUsersState,
-                selectedId: 'PRODUCT-BBB',
+                selectedId: 1,
                 error: ERROR_MSG,
                 loaded: true,
+                filters: {
+                    searchText: 'Leanne'
+                }
             }),
         };
 
@@ -33,10 +30,7 @@ describe('Users Selectors', () => {
         it('selectAllUsers() should return the list of Users', () => {
 
             const results = UsersSelectors.selectAllUsers(state);
-            const selId = getUsersId(results[1]);
-
-            expect(results.length).toBe(3);
-            expect(selId).toBe('PRODUCT-BBB');
+            expect(results.length).toBe(2);
 
         });
 
@@ -53,6 +47,14 @@ describe('Users Selectors', () => {
             const result = UsersSelectors.selectUsersError(state);
 
             expect(result).toBe(ERROR_MSG);
+
+        });
+
+        it('selectFilteredEntities() should filter', () => {
+
+            const result = UsersSelectors.selectFilteredEntities(state);
+
+            expect(result.length).toBe(1);
 
         });
 
